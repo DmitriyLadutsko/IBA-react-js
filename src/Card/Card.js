59c5a29a7fd6704067548b19c424c958f1813166
Dmitriy Ladutsko <dima.ladut@gmail.com>
2020-10-12 14:05:08 +0300

    add: Card and Header components

---
+++ src/Card/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Card.css'
+
+const card = () => {
+    return (
+        <div className="Card">
+            <h3>The Central Nervous System</h3>
+            <p>The brain and spinal cord are the organs of the central nervous system.</p>
+        </div>
+    );
+};
+
+export default card;
---
+++ src/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Header.css'
+
+const header = () => {
+    return (
+        <h1 className="Header">Main Subdivisions</h1>
+    );
+};
+
+export default header;
