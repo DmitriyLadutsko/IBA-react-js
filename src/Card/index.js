f8aa912929d0a8db417245cba0e57880a2ddc78a
Dmitriy Ladutsko <dima.ladut@gmail.com>
2020-10-14 14:50:06 +0300

    add: edit button, react-icons

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,23 +1,36 @@
 import React, {useState} from "react";
 import './Card.css'
+import { FaPencilAlt } from 'react-icons/fa';
 
 const Card = () => {
 
     const [isChecked, setChecked] = useState(false);
 
+    const write = () => {
+        console.log("hi")
+    }
+
     const styleCaption = {
+        paddingLeft: '25px',
+        paddingRight: '25px',
         display: 'flex',
-        justifyContent: 'space-around'
+        justifyContent: 'space-between'
     };
 
     return (
-        <div className="Card" style={{ backgroundColor: isChecked ? '#ebc2f5' : '#c2f5f5' }}>
+        <div className="Card" style={{backgroundColor: isChecked ? '#ebc2f5' : '#c2f5f5'}}>
             <div style={styleCaption}>
                 <h4>The CNS</h4>
-                <input
-                    type="checkbox"
-                    checked={isChecked}
-                    onChange={() => setChecked(!isChecked)}/>
+                <div>
+                    <button onClick={() => write()}>
+                        <FaPencilAlt />
+                    </button>
+                    <input
+                        type="checkbox"
+                        checked={isChecked}
+                        onChange={() => setChecked(!isChecked)}
+                    />
+                </div>
             </div>
             <hr/>
             <p>The brain and spinal cord are the organs of the central nervous system.</p>
