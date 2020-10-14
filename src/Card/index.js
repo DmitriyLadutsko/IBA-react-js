10f8ebae56fd81f5f27af3c06778a98a6fa08820
Dmitriy Ladutsko <dima.ladut@gmail.com>
2020-10-14 11:44:46 +0300

    fix: delete changeStyleCard() - js : card style made inline, checkbox state to the input-tag attribute

--- src/Card/index.js
+++ src/Card/index.js
@@ -5,25 +5,19 @@ const Card = () => {
 
     const [isChecked, setChecked] = useState(false);
 
-    const changeStyleCard = () => {
-        let styleCard = {backgroundColor: '#c2f5f5'};
-        if (isChecked) {
-            styleCard = {backgroundColor: '#ebc2f5'};
-        }
-        return styleCard
-    }
-
-
     const styleCaption = {
         display: 'flex',
         justifyContent: 'space-around'
     };
 
     return (
-        <div className="Card" style={changeStyleCard()}>
+        <div className="Card" style={{ backgroundColor: isChecked ? '#ebc2f5' : '#c2f5f5' }}>
             <div style={styleCaption}>
                 <h4>The CNS</h4>
-                <input type="checkbox" onClick={() => setChecked(!isChecked)}/>
+                <input
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={() => setChecked(!isChecked)}/>
             </div>
             <hr/>
             <p>The brain and spinal cord are the organs of the central nervous system.</p>
