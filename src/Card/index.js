8e10d29aa8ff6f6a60b165c9a2506287c8c457aa
Dmitriy Ladutsko <dima.ladut@gmail.com>
2020-10-14 22:20:40 +0300

    add: input-state to the Card

--- src/Card/index.js
+++ src/Card/index.js
@@ -1,14 +1,37 @@
 import React, {useState} from "react";
-import './Card.css'
-import { FaPencilAlt } from 'react-icons/fa';
+import './Card.css';
+
+import {FaPencilAlt} from 'react-icons/fa';
+import {FaSave} from 'react-icons/fa';
+import {MdClear} from 'react-icons/md';
 
 const Card = () => {
 
     const [isChecked, setChecked] = useState(false);
 
-    const write = () => {
-        console.log("hi")
-    }
+    const [isInput, setInput] = useState(false);
+
+    const [caption, setCaption] = useState({
+        captionValue: 'The CNS'
+    });
+
+    const [text, setText] = useState({
+        textValue: 'The brain and spinal cord are the organs of the central nervous system.'
+    });
+
+    const edit = () => {
+        setChecked(false);
+        setInput(!isInput);
+    };
+
+    let changedCaption;
+    let changedText;
+
+    const getEdited = () => {
+        setCaption({captionValue: changedCaption});
+        setText({textValue: changedText})
+        setInput(!isInput);
+    };
 
     const styleCaption = {
         paddingLeft: '25px',
@@ -18,14 +41,35 @@ const Card = () => {
     };
 
     return (
-        <div className="Card" style={{backgroundColor: isChecked ? '#ebc2f5' : '#c2f5f5'}}>
+        <div className="Card" style={{backgroundColor: isChecked ? '#c2f5f5' : '#ebc2f5'}}>
             <div style={styleCaption}>
-                <h4>The CNS</h4>
+                <h4 style={{display: !isInput ? '' : 'none'}}>{caption.captionValue}</h4>
+                <input
+                    style={{display: isInput ? '' : 'none'}}
+                    type="text"
+                    value={changedCaption}
+                    onChange={event => {
+                        changedCaption = event.target.value
+                    }}
+                />
                 <div>
-                    <button onClick={() => write()}>
-                        <FaPencilAlt />
+                    <button
+                        style={{display: !isInput ? '' : 'none'}}
+                        onClick={edit}>
+                        <FaPencilAlt/>
+                    </button>
+                    <button
+                        style={{display: isInput ? '' : 'none'}}
+                        onClick={getEdited}>
+                        <FaSave/>
+                    </button>
+                    <button
+                        style={{display: isInput ? '' : 'none'}}
+                        onClick={() => setInput(!isInput)}>
+                        <MdClear/>
                     </button>
                     <input
+                        style={{display: !isInput ? '' : 'none'}}
                         type="checkbox"
                         checked={isChecked}
                         onChange={() => setChecked(!isChecked)}
@@ -33,7 +77,15 @@ const Card = () => {
                 </div>
             </div>
             <hr/>
-            <p>The brain and spinal cord are the organs of the central nervous system.</p>
+            <p style={{display: !isInput ? '' : 'none'}}>{text.textValue}</p>
+            <input
+                style={{display: isInput ? '' : 'none'}}
+                type="text"
+                value={changedText}
+                onChange={event => {
+                    changedText = event.target.value
+                }}
+            />
         </div>
     );
 };
